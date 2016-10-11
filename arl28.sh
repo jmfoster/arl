69c23b294716315b07287eb1279ff604a76057c9
James Foster <jmfoster@gmail.com>
2016-10-11 10:24:31 -0600

    forgot to add arl28.sh. run28 -- recruitment2 SI6 yoked to run22 (which was recruitment4)

---
+++ arl28.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --nodes 1
+#SBATCH --ntasks 32
+#SBATCH --output arl28.out
+#SBATCH --qos himem
+
+module load matlab
+module load slurm
+setenv HOME /projects/jmf/arl
+
+matlab -nodesktop -nosplash -r "run"
